4c44c3880ea848f0301a7135d3d5351f129a4d1c
Jeong Wook Oh <oh.jeongwook@gmail.com>
2015-02-27 11:57:23 -0600

    Update Compile.cmd

--- Scripts/Compile.cmd
+++ Scripts/Compile.cmd
@@ -1,4 +1,3 @@
-set PATH=%PATH%;c:\mat\bin\RABCDAsm_v1.16
 mxmlc -omit-trace-statements=false -static-link-runtime-shared-libraries=true -compiler.source-path=. Util.as
 abcexport Util.swf
 rmdir /s /q Util-0
